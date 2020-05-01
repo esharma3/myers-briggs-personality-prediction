424f77f7483d0ec74bb6f18fe4affd2a37dd80aa
Kat Jansen <kat@17cosmicln.net>
2020-04-30 19:46:14 -0500

    Include routes copied from spam detector.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+from joblib import load
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    # list of links to other routes
-    return render_template("index.html")
+# @app.route("/")
+# def home():
+#     # list of links to other routes
+#     return render_template("index.html")
 
 @app.route("/predict")
 def predict():
@@ -14,6 +15,28 @@ def predict():
 @app.route("/analysis")
 def analysis():
     return  render_template("index.html")
+
+@app.route("/")
+def status():
+    return "Ready!"
+
+{
+    "text": "message"
+}
+
+{
+    "result": "ham"
+}
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data_dict = request.get_json()
+
+    text = [data_dict["text"]]
+
+    return jsonify({
+        "result": clf.predict(text)[0]
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
