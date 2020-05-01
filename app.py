965eefa898abbe5395f859f46ba361a0a2459974
Kat Jansen <kat@17cosmicln.net>
2020-04-30 19:47:22 -0500

    Remove duplicate routes caused by copy. reorder routes. add model variable.

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 from joblib import load
 
+model = load("clf.joblib")
+
 app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     # list of links to other routes
-#     return render_template("index.html")
-
-@app.route("/predict")
-def predict():
-    return render_template("index.html")
-
-@app.route("/analysis")
-def analysis():
-    return  render_template("index.html")
 
 @app.route("/")
 def status():
@@ -35,8 +25,12 @@ def predict():
     text = [data_dict["text"]]
 
     return jsonify({
-        "result": clf.predict(text)[0]
+        "result": model.predict(text)[0]
     })
+
+@app.route("/analysis")
+def analysis():
+    return  render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
